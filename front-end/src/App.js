5a0d32f3ee9d171fa0a277c237f190ed22e7cdf3
Thomas Hindermann <thomas.hindermann@ost.ch>
2021-03-10 15:32:40 +0100

    update title & favicon

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,18 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Bild_21_-_Verbot_der_%C3%9Cberschreitung_bestimmter_Fahrgeschwindigkeiten_-_30_km%2C_StVO_1953.svg/600px-Bild_21_-_Verbot_der_%C3%9Cberschreitung_bestimmter_Fahrgeschwindigkeiten_-_30_km%2C_StVO_1953.svg.png" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://tmh-images-live-prod.azureedge.net/formatBackgroundCharacters/9c111ce1-6869-42cb-837a-dd6fd0c7693b_300" className="App-logo" alt="logo" />
       </header>
     </div>
   );
