d67e056b15f0263fd4b28aebaded1aa720211104
Thomas Hindermann <thomas.hindermann@ost.ch>
2021-04-28 14:17:42 +0200

    test connection

--- front-end/src/businessLogic/AuthService.js
+++ front-end/src/businessLogic/AuthService.js
@@ -1,18 +1,20 @@
-export class AuthService {
-    
-    async signIn(username, password) {
-        return {
-            jwt: 'qwertzuikjhgfcvbjiugbn',
-            error: '',
-        };
-    }
+import DataResource from "./dataResource";
 
-    async signUp(username, email, password) {
-        return {
-            jwt: 'qwertzuikjhgfcvbjiugbn',
-            error: '',
-        };
-    }
+export class AuthService {
+  dataResource = new DataResource();
+
+  async signIn(name, password) {
+    return this.dataResource
+      .postJson(`/login`, { name, password })
+      .then((response) => response.json());
+  }
+
+  async signUp(name, password) {
+    console.log("authservice");
+    return this.dataResource
+      .postJson(`/register`, { name, password })
+      .then((response) => response.json());
+  }
 }
 
 export const authService = new AuthService();
